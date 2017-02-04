febe09513501858a9a0ab0655ac778f244f67928
hyst329 <trolleybus.1329@gmail.com>
2017-02-04 21:29:11 +0300

    added translations for settings dialog

--- OpenFool_en.ts
+++ OpenFool_en.ts
@@ -45,97 +45,146 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="14"/>
+        <location filename="mainwindow.cpp" line="17"/>
         <source>Game status</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="33"/>
+        <location filename="mainwindow.cpp" line="36"/>
         <source>Stats isn&apos;t yet implemented...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="34"/>
+        <location filename="mainwindow.cpp" line="37"/>
         <source>But it is planned in future versions.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="43"/>
+        <location filename="mainwindow.cpp" line="50"/>
         <source>Help system is actually absent</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="44"/>
+        <location filename="mainwindow.cpp" line="51"/>
         <source>You will be redirected to Wikipedia page</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="settingsdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="24"/>
+        <source>Rules</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="29"/>
+        <source>Deck</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="34"/>
+        <source>Players</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="56"/>
+        <source>Player 1 (human) name:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="79"/>
+        <source>Player 2 (left opponent) name:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="102"/>
+        <source>Player 3 (partner) name:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="125"/>
+        <source>Player 4 (right opponent) name:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="131"/>
+        <source>Appearance</source>
         <translation></translation>
     </message>
 </context>
 <context>
     <name>Table</name>
     <message>
-        <location filename="table.cpp" line="75"/>
+        <location filename="table.cpp" line="77"/>
         <source>Action</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="160"/>
+        <location filename="table.cpp" line="162"/>
+        <location filename="table.cpp" line="262"/>
         <source>%1&apos;s turn</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="198"/>
-        <location filename="table.cpp" line="262"/>
+        <location filename="table.cpp" line="200"/>
+        <location filename="table.cpp" line="264"/>
         <source>Take</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="204"/>
-        <location filename="table.cpp" line="268"/>
+        <location filename="table.cpp" line="206"/>
+        <location filename="table.cpp" line="270"/>
         <source>Done</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>Game is not over</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>You win</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>You lose</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>Draw</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Spades</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Clubs</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Diamonds</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Hearts</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="table.cpp" line="532"/>
+        <location filename="table.cpp" line="533"/>
         <source>Trump: %1</source>
         <translation></translation>
     </message>
--- OpenFool_ru.ts
+++ OpenFool_ru.ts
@@ -45,97 +45,146 @@
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="14"/>
+        <location filename="mainwindow.cpp" line="17"/>
         <source>Game status</source>
         <translation>Статус игры</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="33"/>
+        <location filename="mainwindow.cpp" line="36"/>
         <source>Stats isn&apos;t yet implemented...</source>
         <translation>Статистика еще не реализована...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="34"/>
+        <location filename="mainwindow.cpp" line="37"/>
         <source>But it is planned in future versions.</source>
         <translation>Она запланирована в будущих версиях.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="43"/>
+        <location filename="mainwindow.cpp" line="50"/>
         <source>Help system is actually absent</source>
         <translation>Справочная система отсутсвует</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="44"/>
+        <location filename="mainwindow.cpp" line="51"/>
         <source>You will be redirected to Wikipedia page</source>
         <translation>Вы будете перенаправлены на страницу в Википедии</translation>
     </message>
 </context>
 <context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="settingsdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="24"/>
+        <source>Rules</source>
+        <translation>Правила</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="29"/>
+        <source>Deck</source>
+        <translation>Колода</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="34"/>
+        <source>Players</source>
+        <translation>Игроки</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="56"/>
+        <source>Player 1 (human) name:</source>
+        <translation>Имя игрока 1 (человек):</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="79"/>
+        <source>Player 2 (left opponent) name:</source>
+        <translation>Имя игрока 2 (оппонент слева):</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="102"/>
+        <source>Player 3 (partner) name:</source>
+        <translation>Имя игрока 3 (партнер):</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="125"/>
+        <source>Player 4 (right opponent) name:</source>
+        <translation>Имя игрока 4 (оппонент справа):</translation>
+    </message>
+    <message>
+        <location filename="settingsdialog.ui" line="131"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+</context>
+<context>
     <name>Table</name>
     <message>
-        <location filename="table.cpp" line="75"/>
+        <location filename="table.cpp" line="77"/>
         <source>Action</source>
         <translation>Действие</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="160"/>
+        <location filename="table.cpp" line="162"/>
+        <location filename="table.cpp" line="262"/>
         <source>%1&apos;s turn</source>
         <translation>Ходит %1</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="198"/>
-        <location filename="table.cpp" line="262"/>
+        <location filename="table.cpp" line="200"/>
+        <location filename="table.cpp" line="264"/>
         <source>Take</source>
         <translation>Беру</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="204"/>
-        <location filename="table.cpp" line="268"/>
+        <location filename="table.cpp" line="206"/>
+        <location filename="table.cpp" line="270"/>
         <source>Done</source>
         <translation>Бито</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>Game is not over</source>
         <translation>Игра не закончена</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>You win</source>
         <translation>Вы выиграли</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>You lose</source>
         <translation>Вы проиграли</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="279"/>
+        <location filename="table.cpp" line="281"/>
         <source>Draw</source>
         <translation>Ничья</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Spades</source>
         <translation>Пики</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Clubs</source>
         <translation>Трефы</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Diamonds</source>
         <translation>Бубны</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="530"/>
+        <location filename="table.cpp" line="531"/>
         <source>Hearts</source>
         <translation>Черви</translation>
     </message>
     <message>
-        <location filename="table.cpp" line="532"/>
+        <location filename="table.cpp" line="533"/>
         <source>Trump: %1</source>
         <translation>Козырь: %1</translation>
     </message>
