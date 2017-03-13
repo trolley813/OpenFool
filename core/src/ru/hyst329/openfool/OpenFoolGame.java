2d074d70453864e9483b5403d8faf05d5429c6c3
hyst329 <trolleybus.1329@gmail.com>
2017-03-13 17:27:32 +0300

    removed HTML target; added test card loading

--- core/src/ru/hyst329/openfool/OpenFoolGame.java
+++ core/src/ru/hyst329/openfool/OpenFoolGame.java
@@ -2,16 +2,37 @@ package ru.hyst329.openfool;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Locale;
 
 public class OpenFoolGame extends ApplicationAdapter {
     SpriteBatch batch;
+    AssetManager assetManager;
+    BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        assetManager = new AssetManager();
+        font = new BitmapFont();
+        font.setColor(Color.CHARTREUSE);
+        String[] decks = {"fra", "int", "rus"};
+        String suits = "cdhs";
+        for (String d : decks) {
+            for (int i = 1; i <= 13; i++) {
+                for (char s : suits.toCharArray()) {
+                    assetManager.load(String.format(Locale.ENGLISH, "%s/%d%s.png", d, i, s),
+                            Texture.class);
+                }
+            }
+        }
+
     }
 
     @Override
@@ -19,6 +40,12 @@ public class OpenFoolGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+        if (assetManager.update()) {
+            batch.draw(assetManager.get("rus/12d.png", Texture.class), 0, 0);
+        } else {
+            font.draw(batch, String.format("Loading %s%%...",
+                    assetManager.getProgress() * 100), 20, 20);
+        }
         batch.end();
     }
 
