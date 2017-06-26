488da0424008e235620961352036a69ac7c5e7b2
hyst329 <trolleybus.1329@gmail.com>
2017-06-26 22:03:35 +0300

    added rules setup; deuce can actually beat ace

--- core/src/ru/hyst329/openfool/Card.kt
+++ core/src/ru/hyst329/openfool/Card.kt
@@ -4,11 +4,12 @@ import java.util.Locale
 
 class Card(internal val suit: Suit, internal val rank: Rank) {
 
-    internal fun beats(other: Card, trumpSuit: Suit): Boolean {
+    internal fun beats(other: Card, trumpSuit: Suit, deuceBeatsAce: Boolean = false): Boolean {
         val thisRankValue = (this.rank.value + 11) % 13
         val otherRankValue = (other.rank.value + 11) % 13
         if (this.suit === other.suit) {
-            return thisRankValue >= otherRankValue
+            // deuce: (2 + 11) % 13 == 0, ace: (1 + 11) % 13 == 12
+            return thisRankValue >= otherRankValue || (deuceBeatsAce && thisRankValue == 0 && otherRankValue == 12)
         } else
             return this.suit === trumpSuit
     }
--- core/src/ru/hyst329/openfool/NewGameScreen.kt
+++ core/src/ru/hyst329/openfool/NewGameScreen.kt
@@ -7,11 +7,17 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.viewport.FitViewport
+import com.kotcrab.vis.ui.widget.VisCheckBox
+import com.kotcrab.vis.ui.widget.VisLabel
 import com.kotcrab.vis.ui.widget.VisTextButton
 
 internal class NewGameScreen(private val game: OpenFoolGame) : Screen {
     private val stage: Stage = Stage(FitViewport(800f, 480f))
     private val singlePlayerButton: VisTextButton
+    private val gameplayLabel: VisLabel
+    private val deuceCheckBox: VisCheckBox
+    private val limitCheckBox: VisCheckBox
+    private val ruleSet: RuleSet = RuleSet(game.preferences)
 
     init {
         Gdx.input.inputProcessor = stage
@@ -24,6 +30,17 @@ internal class NewGameScreen(private val game: OpenFoolGame) : Screen {
             }
         })
         stage.addActor(singlePlayerButton)
+        gameplayLabel = VisLabel(game.localeBundle.get("GameplaySettings"))
+        gameplayLabel.setBounds(440f, 350f, 260f, 40f)
+        stage.addActor(gameplayLabel)
+        deuceCheckBox = VisCheckBox(game.localeBundle.get("DeuceBeatsAce"))
+        deuceCheckBox.setBounds(440f, 300f, 260f, 40f)
+        deuceCheckBox.isChecked = ruleSet.deuceBeatsAce
+        stage.addActor(deuceCheckBox)
+        limitCheckBox = VisCheckBox(game.localeBundle.get("LimitTo5Cards"))
+        limitCheckBox.setBounds(440f, 250f, 260f, 40f)
+        stage.addActor(limitCheckBox)
+        deuceCheckBox.isChecked = ruleSet.loweredFirstDiscardLimit
     }
 
 
